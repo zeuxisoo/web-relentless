d6d44dd386fdef537ae40e46e45600ce4b60b736
Zeuxis Lo <seekstudio@gmail.com>
2021-11-20 21:08:03 +0800

    improve the food name and units id checking to make sure it is created by current user

--- app/Api/Version1/Requests/FoodMenuStoreRequest.php
+++ app/Api/Version1/Requests/FoodMenuStoreRequest.php
@@ -2,6 +2,8 @@
 namespace App\Api\Version1\Requests;
 
 use App\Api\Version1\Bases\ApiRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class FoodMenuStoreRequest extends ApiRequest {
 
@@ -11,12 +13,20 @@ class FoodMenuStoreRequest extends ApiRequest {
 
     public function rules() {
         return [
-            'start_at'             => 'required|date_format:"Y-m-d H:i:s"|after_or_equal:now',
-            'foods'                => 'required|array',
-            'foods.*.food_name_id' => 'required|exists:food_names,id',
-            'foods.*.food_unit_id' => 'required|exists:food_units,id',
-            'tags'                 => 'required|array',
-            'remark'               => '',
+            'start_at' => 'required|date_format:"Y-m-d H:i:s"|after_or_equal:now',
+            'foods'    => 'required|array',
+            'tags'     => 'required|array',
+            'remark'   => '',
+
+            'foods.*.food_name_id' => [
+                'required',
+                Rule::exists('food_names', 'id')->where('user_id', Auth::id()),
+            ],
+
+            'foods.*.food_unit_id' => [
+                'required',
+                Rule::exists('food_units', 'id')->where('user_id', Auth::id()),
+            ],
         ];
     }
 
@@ -26,12 +36,12 @@ class FoodMenuStoreRequest extends ApiRequest {
             'start_at.date_format'          => 'Invalid format of field start at',
             'foods.required'                => 'Please enter foods',
             'foods.array'                   => 'Invalid format of field foods',
+            'tags.required'                 => 'Please enter tags',
+            'tags.array'                    => 'Invalid form of field tags',
             'foods.*.food_name_id.required' => 'Please enter food name',
             'foods.*.food_unit_id.required' => 'Please enter food unit',
             'foods.*.food_name_id.exists'   => 'Food name is not exists',
             'foods.*.food_unit_id.exists'   => 'Food unit is not exists',
-            'tags.required'                 => 'Please enter tags',
-            'tags.array'                    => 'Invalid form of field tags',
         ];
     }
 
