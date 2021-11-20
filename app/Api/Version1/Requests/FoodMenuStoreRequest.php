e8f049b56f86b0ee39ccf72012ccc3786a64fa72
Zeuxis Lo <seekstudio@gmail.com>
2021-11-20 21:02:05 +0800

    add request validation rule on the api.food.menu.store api

--- app/Api/Version1/Requests/FoodMenuStoreRequest.php
+++ app/Api/Version1/Requests/FoodMenuStoreRequest.php
@@ -11,13 +11,27 @@ class FoodMenuStoreRequest extends ApiRequest {
 
     public function rules() {
         return [
-
+            'start_at'             => 'required|date_format:"Y-m-d H:i:s"|after_or_equal:now',
+            'foods'                => 'required|array',
+            'foods.*.food_name_id' => 'required|exists:food_names,id',
+            'foods.*.food_unit_id' => 'required|exists:food_units,id',
+            'tags'                 => 'required|array',
+            'remark'               => '',
         ];
     }
 
     public function messages() {
         return [
-
+            'start_at.required'             => 'Please enter start at',
+            'start_at.date_format'          => 'Invalid format of field start at',
+            'foods.required'                => 'Please enter foods',
+            'foods.array'                   => 'Invalid format of field foods',
+            'foods.*.food_name_id.required' => 'Please enter food name',
+            'foods.*.food_unit_id.required' => 'Please enter food unit',
+            'foods.*.food_name_id.exists'   => 'Food name is not exists',
+            'foods.*.food_unit_id.exists'   => 'Food unit is not exists',
+            'tags.required'                 => 'Please enter tags',
+            'tags.array'                    => 'Invalid form of field tags',
         ];
     }
 
