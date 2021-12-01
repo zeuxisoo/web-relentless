33e5ba0d7734d9d9c5de92cad767dd4ab1b19c3b
Zeuxis Lo <seekstudio@gmail.com>
2021-12-01 23:31:35 +0800

    support pass food name and unit by value not only related id in api.food.menu.store

--- app/Api/Version1/Controllers/FoodMenuController.php
+++ app/Api/Version1/Controllers/FoodMenuController.php
@@ -8,6 +8,9 @@ use App\Api\Version1\Requests\FoodMenuStoreRequest;
 use App\Api\Version1\Transformers\FoodMenuTransformer;
 use App\Models\FoodMenu;
 use App\Models\FoodMenuItem;
+use App\Models\FoodName;
+use App\Models\FoodUnit;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
@@ -27,19 +30,25 @@ class FoodMenuController extends ApiController {
             $foodMenu->attachTags($input['tags'], TagCategory::Food);
 
             // Food menu items
+            $foodNameIds = $this->getOrCreateFieldIdsFromFoods($input['foods'], field: 'name');
+            $foodUnitIds = $this->getOrCreateFieldIdsFromFoods($input['foods'], field: 'unit');
+
             $foodItems = [];
 
             foreach($input['foods'] as $food) {
                 array_push($foodItems, [
                     'user_id'      => Auth::id(),
                     'food_menu_id' => $foodMenu->id,
-                    'food_name_id' => $food['food_name_id'],
-                    'food_unit_id' => $food['food_unit_id'],
+                    'food_name_id' => $foodNameIds[$food['name']],
+                    'food_unit_id' => $foodUnitIds[$food['unit']],
                     'quantity'     => $food['quantity'],
                 ]);
             }
 
             FoodMenuItem::insert($foodItems);
+
+            // Load the relationship first
+            $foodMenu->load('foodItems.foodName', 'foodItems.foodUnit');
 
             return $foodMenu;
         });
@@ -48,9 +57,70 @@ class FoodMenuController extends ApiController {
     }
 
     public function show(FoodMenuShowRequest $id) {
-        $foodMenu = FoodMenu::find($id);
+        $foodMenu = FoodMenu::with('foodItems.foodName', 'foodItems.foodUnit')->find($id);
 
         return fractal($foodMenu, new FoodMenuTransformer());
+    }
+
+    //
+    protected function getOrCreateFieldIdsFromFoods(array $foods, string $field): array {
+        // Select model
+        $models = [
+            'name' => new FoodName(),
+            'unit' => new FoodUnit(),
+        ];
+
+        $model = $models[$field];
+
+        // Convert name list from foods by field name
+        $names = $this->getFieldNamesFromFoods($foods, $field);
+
+        // Get the exists names by name list
+        $existsNames = $model::select('name')
+            ->whereIn('name', $names)
+            ->where('user_id', Auth::id())
+            ->pluck('name')
+            ->toArray();
+
+        // Get the not exists names by compare exists names
+        $notExistsNames = array_diff($names, $existsNames);
+
+        // Bulk insert the not exists names into database
+        if (!empty($notExistsNames)) {
+            $notExistsNames = collect($notExistsNames)
+                ->map(fn($name) => [
+                    'user_id'    => Auth::id(),
+                    'name'       => $name,
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                ])
+                ->toArray();
+
+            $model::insert($notExistsNames);
+        }
+
+        // Re-get the latest exists names and covert to `name: id` structure
+        $ids = $model::select('id', 'name')
+            ->whereIn('name', $names)
+            ->get()
+            ->mapWithKeys(fn($row) => [
+                $row->name => $row->id
+            ])
+            ->toArray();
+
+        return $ids;
+    }
+
+    protected function getFieldNamesFromFoods(array $foods, string $field): array {
+        $names = [];
+
+        foreach($foods as $food) {
+            if (!in_array($food[$field], $names)) {
+                array_push($names, $food[$field]);
+            }
+        }
+
+        return $names;
     }
 
 }
--- app/Api/Version1/Requests/FoodMenuStoreRequest.php
+++ app/Api/Version1/Requests/FoodMenuStoreRequest.php
@@ -18,35 +18,26 @@ class FoodMenuStoreRequest extends ApiRequest {
             'tags'     => 'required|array',
             'remark'   => '',
 
-            'foods.*.food_name_id' => [
-                'required',
-                Rule::exists('food_names', 'id')->where('user_id', Auth::id()),
-            ],
-
-            'foods.*.food_unit_id' => [
-                'required',
-                Rule::exists('food_units', 'id')->where('user_id', Auth::id()),
-            ],
-
+            'foods.*.name'     => 'required',
+            'foods.*.unit'     => 'required',
             'foods.*.quantity' => 'required|integer|gt:0',
         ];
     }
 
     public function messages() {
         return [
-            'start_at.required'             => __('Please enter start at'),
-            'start_at.date_format'          => __('Invalid format of field start at'),
-            'foods.required'                => __('Please enter foods'),
-            'foods.array'                   => __('Invalid format of field foods'),
-            'tags.required'                 => __('Please enter tags'),
-            'tags.array'                    => __('Invalid form of field tags'),
-            'foods.*.food_name_id.required' => __('Please enter food name'),
-            'foods.*.food_unit_id.required' => __('Please enter food unit'),
-            'foods.*.food_name_id.exists'   => __('Food name is not exists'),
-            'foods.*.food_unit_id.exists'   => __('Food unit is not exists'),
-            'foods.*.quantity.required'     => __('Please enter food quantity'),
-            'foods.*.quantity.integer'      => __('Food quantity must be integer'),
-            'foods.*.quantity.gt'           => __('Food quantity must gether than zero'),
+            'start_at.required'    => __('Please enter start at'),
+            'start_at.date_format' => __('Invalid format of field start at'),
+            'foods.required'       => __('Please enter foods'),
+            'foods.array'          => __('Invalid format of field foods'),
+            'tags.required'        => __('Please enter tags'),
+            'tags.array'           => __('Invalid form of field tags'),
+
+            'foods.*.name.required'     => __('Please enter food name'),
+            'foods.*.unit.required'     => __('Please enter food unit'),
+            'foods.*.quantity.required' => __('Please enter food quantity'),
+            'foods.*.quantity.integer'  => __('Food quantity must be integer'),
+            'foods.*.quantity.gt'       => __('Food quantity must gether than zero'),
         ];
     }
 
