b9c0cb412f18c78ac0680796870996bcd7ba6d9a
Zeuxis Lo <seekstudio@gmail.com>
2021-12-28 22:19:54 +0800

    add custom searchable trait to food menu model

--- app/Models/FoodMenu.php
+++ app/Models/FoodMenu.php
@@ -1,15 +1,17 @@
 <?php
 namespace App\Models;
 
+use App\Api\Version1\Services\FoodMenuItemService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Scout\Searchable;
 use Spatie\Tags\HasTags;
 
 class FoodMenu extends Model {
 
-    use SoftDeletes, HasFactory, HasTags;
+    use SoftDeletes, HasFactory, HasTags, Searchable;
 
     protected $fillable = ['user_id', 'start_at', 'remark'];
 
@@ -27,6 +29,21 @@ class FoodMenu extends Model {
 
     public function foods() {
         return $this->hasMany(FoodMenuItem::class);
+    }
+
+    // Scout
+    public function searchableAs() {
+        return 'food_menus_index';
+    }
+
+    public function toSearchableArray() {
+        $foodMenu = $this->toArray();
+
+        $foodMenu['foods'] = (new FoodMenuItemService())
+            ->getByFoodMenuId($this->id)
+            ->toArray();
+
+        return $foodMenu;
     }
 
 }
