2111eff7b22ea2631fcbf778bb038202281ef212
Zeuxis Lo <seekstudio@gmail.com>
2022-03-20 21:17:55 +0800

    merge the preview method using the new compile method in food parser helper class

--- app/Api/Version1/Controllers/FoodMenuNoteController.php
+++ app/Api/Version1/Controllers/FoodMenuNoteController.php
@@ -4,24 +4,14 @@ namespace App\Api\Version1\Controllers;
 use App\Api\Version1\Bases\ApiController;
 use App\Api\Version1\Requests\FoodMenuNotePreviewRequest;
 use App\Api\Version1\Transformers\FoodMenuNoteTransformer;
-use App\Parsers\Food\{
-    Lexer,
-    Parser,
-    Traverser,
-    Generator,
-};
+use App\Parsers\Food\Helper as FoodParserHelper;
 
 class FoodMenuNoteController extends ApiController {
 
     public function preview(FoodMenuNotePreviewRequest $request) {
         $input = $request->only('text');
 
-        $lexer     = new Lexer($input['text']);
-        $parser    = new Parser($lexer);
-        $traverser = new Traverser($parser);
-        $generator = new Generator($traverser);
-
-        $foods = $generator->generate();
+        $foods = FoodParserHelper::compile($input['text']);
 
         return fractal($foods, new FoodMenuNoteTransformer());
     }
