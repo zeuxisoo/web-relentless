e116ac62d051273dd559d60fcae60356fb485d60
Zeuxis Lo <seekstudio@gmail.com>
2022-03-31 15:52:10 +0800

    change the food menu note transaction to block syntax in create method

--- app/Api/Version1/Services/FoodMenuNoteService.php
+++ app/Api/Version1/Services/FoodMenuNoteService.php
@@ -12,31 +12,31 @@ class FoodMenuNoteService {
     ) {}
 
     public function create(array $foodMenus): array {
-        DB::beginTransaction();
+        $menus = DB::transaction(function() use ($foodMenus) {
+            $menus = [];
 
-        $menus = [];
+            foreach($foodMenus as $foodMenu) {
+                // Food menu
+                $newMenu = FoodMenu::create([
+                    'start_at' => sprintf('%s %s', $foodMenu['date'], $foodMenu['time']),
+                    'remark'   => $foodMenu['remark'],
+                ]);
 
-        foreach($foodMenus as $foodMenu) {
-            // Food menu
-            $newMenu = FoodMenu::create([
-                'start_at' => sprintf('%s %s', $foodMenu['date'], $foodMenu['time']),
-                'remark'   => $foodMenu['remark'],
-            ]);
+                // Food menu tags
+                $newMenu->attachTags($foodMenu['tags'], TagCategory::Food);
 
-            // Food menu tags
-            $newMenu->attachTags($foodMenu['tags'], TagCategory::Food);
+                // Food menu items
+                $this->foodMenuItemService->insert($foodMenu['foods'], $newMenu);
 
-            // Food menu items
-            $this->foodMenuItemService->insert($foodMenu['foods'], $newMenu);
+                // Load the food items relationship first after items created
+                $newMenu->foods = $this->foodMenuItemService->getByFoodMenuId($newMenu->id);
 
-            // Load the food items relationship first after items created
-            $newMenu->foods = $this->foodMenuItemService->getByFoodMenuId($newMenu->id);
+                //
+                $menus[] = $newMenu;
+            }
 
-            //
-            $menus[] = $newMenu;
-        }
-
-        DB::commit();
+            return $menus;
+        });
 
         return $menus;
     }
